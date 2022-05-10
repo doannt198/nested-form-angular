a3d567ecb16fd3be6eceb0d4f17e01cfb1d8e3dd
doannt <doannt@unicloud.com.vn>
2022-05-10 17:50:45 +0700

    update

--- src/app/components/home/reactive-form-hai/reactive-form-hai.component.ts
+++ src/app/components/home/reactive-form-hai/reactive-form-hai.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form-hai',
@@ -6,10 +7,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive-form-hai.component.scss']
 })
 export class ReactiveFormHaiComponent implements OnInit {
-
-  constructor() { }
+  submitForm: FormGroup
+  constructor(private fb :FormBuilder) { }
 
   ngOnInit(): void {
+    this.submitForm =this.fb.group({
+        hoTen: ['', [Validators.required]],
+        diaChi: ['',[Validators.required]],
+        dienThoai: [''],
+        email: ['',[Validators.required]],
+        check: [false],
+        soThich: this.fb.array([]),
+        congTy:this.fb.group({
+          tenCongTy: ['', [Validators.required]],
+          diaChi: ['', [Validators.required]]
+        }),
+        gioiTinh: ['']
+    })
+  }
+  get soThich():FormArray {
+    return this.submitForm.get('soThich') as FormArray
+  }
+   
+  get checkRequired() {
+    return this.submitForm.get('check')
+  }
+  check() {
+    console.log(this.checkRequired?.value)
+    if(this.checkRequired?.value) {
+        this.submitForm.controls.dienThoai.setValidators([Validators.required])
+    }else {
+        this.submitForm.controls.dienThoai.clearValidators()
+    }
+    this.submitForm.controls.dienThoai.updateValueAndValidity({ emitEvent:false })
   }
 
+  soThichItem() {
+    return this.fb.group({
+        tenCongTy: ['', [Validators.required]],
+        diaChi: ['', [Validators.required]]
+    })
+  }
+
+  addSoThich() {
+    this.soThich.push(this.soThichItem())
+  }
+
+  removeSoThich( i:any) {
+      this.soThich.removeAt(i)
+  }
+
+  onSubmit(submitForm:any ) {
+    console.log( submitForm.value)
+    this.submitForm.reset();
+  }
 }
