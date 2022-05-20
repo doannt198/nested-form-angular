ab2dd7ba258d128cdf79bf6ed7519307a353e72f
doannt <doannt@unicloud.com.vn>
2022-05-20 07:33:00 +0700

    a

--- src/app/components/home/array/array.component.ts
+++ src/app/components/home/array/array.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-array',
@@ -8,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class ArrayComponent implements OnInit {
 
   constructor() { }
+  @Input() value : any
 
   ngOnInit(): void {
+    
   }
 
 }
--- src/app/components/home/object/object.component.ts
+++ src/app/components/home/object/object.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ArrayComponent } from '../array/array.component';
 
 @Component({
   selector: 'app-object',
@@ -8,8 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class ObjectComponent implements OnInit {
 
   constructor() { }
-
+  giatri=0
   ngOnInit(): void {
   }
+
 
 }
