26f683847189d734b4ae09790bf172e8c9d49f5a
doannt <doannt@unicloud.com.vn>
2022-06-24 10:33:21 +0700

    up

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+const urlApi="https://tano-api.herokuapp.com"
+
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
+  
   constructor(private http: HttpClient) { }
 
   getListProduct():Observable<any>{
-    return this.http.get<any>('https://tano-api.herokuapp.com/products')
+    return this.http.get<any>(`${urlApi}/products`)
   }
   addProduct(data:string):Observable<any>{
-    return this.http.post<any>('https://tano-api.herokuapp.com/products/add',data )
+    return this.http.post<any>(`${urlApi}/products/add`,data )
   }
   deleteProduct(id:number):Observable<any>{
-    return this.http.delete<any>(`https://tano-api.herokuapp.com/products/delete/${id}`)
+    return this.http.delete<any>(`${urlApi}/products/delete/${id}`)
   }
   getDetail(id:number):Observable<any>{
-    return this.http.get<any>(` https://tano-api.herokuapp.com/products/${id}`)
+    return this.http.get<any>(`${urlApi}/products/${id}`)
   }
   editProduct(data:any):Observable<any>{
-    return this.http.post<any>('https://tano-api.herokuapp.com/products/edit',data)
+    return this.http.post<any>(`${urlApi}/products/edit`,data)
   }
 }
