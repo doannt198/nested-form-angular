867219b8806569f5cbf6a17bae1b0f9f0e22a95a
doannt <doannt@unicloud.com.vn>
2022-06-28 16:52:28 +0700

    update auth

--- src/app/services/auth-guard.guard.ts
+++ src/app/services/auth-guard.guard.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardGuard implements CanActivate {
+
+  constructor(
+    public router: Router
+  ) {
+
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       if(localStorage.getItem('user')) {
           return true
+         
       } else {
+        this.router.navigate([''])
         return false;
       }
   }
