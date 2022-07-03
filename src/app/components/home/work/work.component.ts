8757dfc36d77ab2dda79141de738317fb9449e70
doannt <doannt@unicloud.com.vn>
2022-07-04 05:52:49 +0700

    up

--- src/app/components/home/work/work.component.ts
+++ src/app/components/home/work/work.component.ts
@@ -15,8 +15,12 @@ export class WorkComponent implements OnInit {
   showmesage = false
   results:any;
   resultSearch: any;
+  resultSearchNhiPhan: any
+  keyword: any;
+  number: Array<any>;
   ngOnInit(): void {
   this.getPhepNam()
+  this.number = [1,3,4,5,7,8,9,11]
   }
 
   getPhepNam():void {
@@ -27,6 +31,12 @@ export class WorkComponent implements OnInit {
     })
   }
 
+  show() {
+    this.resultSearch = this.linearSearch(this.results, this.value2)
+    console.log(this.resultSearch)
+    this.showmesage = true
+  }
+
   linearSearch(arr: any , key: string) {
     for(let i =0; i< arr.length;i++) {
       if(arr[i].full_name ===key) {
@@ -35,10 +45,27 @@ export class WorkComponent implements OnInit {
     }
     return -1
   }
-
-  show() {
-    this.resultSearch = this.linearSearch(this.results, this.value2)
-    console.log(this.resultSearch)
-    this.showmesage = true
+/*  Thuật toán sắp sếp nhị phân */
+  show2() {
+   this.resultSearchNhiPhan = this.binarySearch(this.number, this.keyword)
+   console.log(this.resultSearchNhiPhan)
   }
-}
+ 
+    binarySearch(nums: Array<number>, key: number): number {
+      let low = 0;
+      let high = nums.length - 1;
+      while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        mid;
+        if (nums[mid] == key) {
+          return mid ;
+        }
+        if (key > nums[mid]) {
+          low = mid + 1;
+        } else {
+          high = mid - 1;
+        }
+      }
+      return -1;
+    }
+ }
