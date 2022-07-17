81e5b8e2d55d667710ee3923778fd93db3f846e7
doannt <doannt@unicloud.com.vn>
2022-07-17 20:59:54 +0700

    upda

---
+++ src/app/components/home/upload-anh/upload-anh.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-anh',
+  templateUrl: './upload-anh.component.html',
+  styleUrls: ['./upload-anh.component.scss']
+})
+export class UploadAnhComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
